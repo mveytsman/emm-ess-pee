f58656eb2929ad146fffa0dfe18e0fac2376ac48
Max Veytsman <maxim@ontoillogical.com>
2014-06-23 20:25:41 -0400

    Redid sample program

--- resources/sample_programs/hello_world.c
+++ resources/sample_programs/hello_world.c
@@ -1,14 +1,23 @@
 #include <msp430.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#include <io.h>
+
+
  
 int main(void)
 {
-    printf("Hello World\n");
+    //printf("Hello World\n");
+    char* lol = malloc(100);
+    lol[2] = 'a';
     return 0;
 }
  
-int  putchar(int c)
-{
-    // Write a driver here that writes c somewhere
-    return 1;
-}
+/* int  putchar(int c) */
+/* { */
+/*     //while (!(UTCTL0 & TXEPT)) {}   // wait util tx buffer is free  */
+/*     //TXBUF0 = character;            // send character  */
+/*     return 1;  */
+/* } */
